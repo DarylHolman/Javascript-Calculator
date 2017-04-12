4fac6f42f8d5ac14a262f558a5d6c7a71c43a7d1
DarylHolman <dholmanj@gmail.com>
2017-04-11 20:55:03 -0400

    Class Exercise Calculator
    Apr 11 Calc

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(){
+	//console.log("Let's add");
+	console.log(9 + 9)
+}
+function subtract(){
+	//console.log("Let's subtract");
+	console.log(9 - 9)
+}
+function Multiply(){
+	//console.log("Let's multiply");
+	console.log(9 * 9)
+}
+function Divide(){
+	//console.log("Issa divide");
+	console.log(9 / 9)
+}
